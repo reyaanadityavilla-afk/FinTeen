eb61086dd4756778c88fd3553d36e310cd16e6b7
reyaanadityavilla-afk <reyaanadityavilla@gmail.com>
2026-06-19 13:49:03 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FINTEEN_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDsLRbbXbkzD8QJqOFbphNjJ8O581UprqI",
+  authDomain: "finteen26.firebaseapp.com",
+  projectId: "finteen26",
+  storageBucket: "finteen26.firebasestorage.app",
+  messagingSenderId: "835662472464",
+  appId: "1:835662472464:web:aba7470cb3ff74012a7195"
+};
